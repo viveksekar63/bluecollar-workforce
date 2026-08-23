1203573f6094cbb607edb9544531e6ea42a4a56b
viveksekar63 <65102446+viveksekar63@users.noreply.github.com>
2026-08-23 15:51:01 +0530

    feat(documents): initialize worker document module

--- backend/src/documents/documents.module.ts
+++ backend/src/documents/documents.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 
-@Module({})
+import { AuthModule } from "../auth/auth.module";
+import { DocumentsController } from "./documents.controller";
+import { DocumentsService } from "./documents.service";
+
+@Module({
+  imports: [AuthModule],
+  controllers: [DocumentsController],
+  providers: [DocumentsService],
+  exports: [DocumentsService],
+})
 export class DocumentsModule {}
