cf7115b9a800db1017212980d1c2ae127ee9452f
viveksekar63 <65102446+viveksekar63@users.noreply.github.com>
2026-08-24 15:09:36 +0530

    feat(employer): register applicant profile service

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JobsController } from './jobs.controller';
+import { EmployerApplicationService } from './employer-application.service';
 import { JobsService } from './jobs.service';
 
 @Module({
   controllers: [JobsController],
-  providers: [JobsService]
+  providers: [JobsService, EmployerApplicationService],
 })
 export class JobsModule {}
