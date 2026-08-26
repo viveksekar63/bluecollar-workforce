7b979d6cd4f49a332d85ff276f7e2005fb5e1714
viveksekar63 <65102446+viveksekar63@users.noreply.github.com>
2026-08-26 14:12:41 +0530

    feat(payments): register employer payment service

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JobsController } from './jobs.controller';
 import { EmployerApplicationService } from './employer-application.service';
+import { EmployerPaymentService } from './employer-payment.service';
 import { JobsService } from './jobs.service';
 
 @Module({
   controllers: [JobsController],
-  providers: [JobsService, EmployerApplicationService],
+  providers: [JobsService, EmployerApplicationService, EmployerPaymentService],
 })
 export class JobsModule {}
