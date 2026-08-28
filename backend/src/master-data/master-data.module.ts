1ef3dbed86247bcdfb8f63bc86d415c0343ef3d2
viveksekar63 <65102446+viveksekar63@users.noreply.github.com>
2026-08-28 09:34:17 +0530

    fix: provide permission dependencies to master data

--- backend/src/master-data/master-data.module.ts
+++ backend/src/master-data/master-data.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MasterDataController } from './master-data.controller';
 import { MasterDataService } from './master-data.service';
+import { PermissionModule } from '../auth/permissions/permission.module';
 
 @Module({
+  imports: [PermissionModule],
   controllers: [MasterDataController],
   providers: [MasterDataService],
   exports: [MasterDataService],
