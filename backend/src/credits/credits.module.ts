dd9aa09fe6e95250a5bf9f47c4fb7eb99ac650ea
viveksekar63 <65102446+viveksekar63@users.noreply.github.com>
2026-08-28 07:58:55 +0530

    feat: export credit wallet service

--- backend/src/credits/credits.module.ts
+++ backend/src/credits/credits.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CreditsController } from './credits.controller';
 import { CreditsService } from './credits.service';
+import { CreditWalletService } from '../contact-purchases/credit-wallet.service';
 
-@Module({ controllers: [CreditsController], providers: [CreditsService], exports: [CreditsService] })
+@Module({
+  controllers: [CreditsController],
+  providers: [CreditsService, CreditWalletService],
+  exports: [CreditsService, CreditWalletService],
+})
 export class CreditsModule {}
