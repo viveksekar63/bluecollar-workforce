9a800bb83d290106229479e901cd1bcbfe1bf848
viveksekar63 <65102446+viveksekar63@users.noreply.github.com>
2026-09-07 15:56:59 +0530

    feat: register recruitment outreach module services

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -3,6 +3,8 @@ import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { AiWorkerActionsController } from './ai-worker-actions.controller';
 import { AiWorkerActionsService } from './ai-worker-actions.service';
+import { RecruitmentOutreachController } from './recruitment-outreach.controller';
+import { RecruitmentOutreachService } from './recruitment-outreach.service';
 import { JobsController } from './jobs.controller';
 import { EmployerApplicationService } from './employer-application.service';
 import { EmployerPaymentService } from './employer-payment.service';
@@ -10,13 +12,14 @@ import { JobsService } from './jobs.service';
 
 @Module({
   imports: [SubscriptionsModule, NotificationsModule],
-  controllers: [JobsController, AiWorkerActionsController],
+  controllers: [JobsController, AiWorkerActionsController, RecruitmentOutreachController],
   providers: [
     JobsService,
     EmployerApplicationService,
     EmployerPaymentService,
     AiWorkerActionsService,
+    RecruitmentOutreachService,
   ],
-  exports: [JobsService, AiWorkerActionsService],
+  exports: [JobsService, AiWorkerActionsService, RecruitmentOutreachService],
 })
 export class JobsModule {}
