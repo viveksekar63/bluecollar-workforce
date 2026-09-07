11d1e8a021336b45c14371430cd2662275b78b5e
viveksekar63 <65102446+viveksekar63@users.noreply.github.com>
2026-09-07 16:42:01 +0530

    feat: register recruitment autopilot module components

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -5,6 +5,8 @@ import { AiWorkerActionsController } from './ai-worker-actions.controller';
 import { AiWorkerActionsService } from './ai-worker-actions.service';
 import { RecruitmentOutreachController } from './recruitment-outreach.controller';
 import { RecruitmentOutreachService } from './recruitment-outreach.service';
+import { RecruitmentAutopilotController } from './recruitment-autopilot.controller';
+import { RecruitmentAutopilotService } from './recruitment-autopilot.service';
 import { JobsController } from './jobs.controller';
 import { EmployerApplicationService } from './employer-application.service';
 import { EmployerPaymentService } from './employer-payment.service';
@@ -12,14 +14,15 @@ import { JobsService } from './jobs.service';
 
 @Module({
   imports: [SubscriptionsModule, NotificationsModule],
-  controllers: [JobsController, AiWorkerActionsController, RecruitmentOutreachController],
+  controllers: [JobsController, AiWorkerActionsController, RecruitmentOutreachController, RecruitmentAutopilotController],
   providers: [
     JobsService,
     EmployerApplicationService,
     EmployerPaymentService,
     AiWorkerActionsService,
     RecruitmentOutreachService,
+    RecruitmentAutopilotService,
   ],
-  exports: [JobsService, AiWorkerActionsService, RecruitmentOutreachService],
+  exports: [JobsService, AiWorkerActionsService, RecruitmentOutreachService, RecruitmentAutopilotService],
 })
 export class JobsModule {}
